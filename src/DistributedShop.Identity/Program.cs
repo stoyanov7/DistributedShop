3ce666a40b9ba8d9b9487b0a2582e42612b8de2e
stoyanov7 <georgi.stoyanov777@gmail.com>
2021-12-15 14:31:52 +0200

    init identity project

---
+++ src/DistributedShop.Identity/Program.cs
@@ -0,0 +1,16 @@
+namespace DistributedShop.Identity
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .Build()
+                .Run();
+        }
+    }
+}
