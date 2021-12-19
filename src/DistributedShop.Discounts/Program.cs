55f21d40f784c05c6741ee5bbac1cc5d94fa42a8
stoyanov7 <georgi.stoyanov777@gmail.com>
2021-12-19 14:53:23 +0200

    initialize discount project

---
+++ src/DistributedShop.Discounts/Program.cs
@@ -0,0 +1,14 @@
+namespace DistributedShop.Discounts
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+            => Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .Build()
+                .Run();
+    }
+}
