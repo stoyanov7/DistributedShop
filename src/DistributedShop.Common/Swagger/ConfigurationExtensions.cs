f50419d71d1e7329fa7aa2152b2b3a89db5430fe
stoyanov7 <georgi.stoyanov777@gmail.com>
2021-12-19 14:47:23 +0200

    service for swagger

---
+++ src/DistributedShop.Common/Swagger/ConfigurationExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace DistributedShop.Common.Swagger
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static partial class ConfigurationExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services, IConfiguration configuration)
+        {
+            var options = configuration.GetOptions<SwaggerSettings>();
+
+            return !options.Enabled ? services : services.AddSwaggerGen();
+        }
+
+        public static IApplicationBuilder UseSwagger(this IApplicationBuilder builder)
+        {
+            var options = builder
+                .ApplicationServices
+                .GetRequiredService<IConfiguration>()
+                .GetOptions<SwaggerSettings>();
+
+            if (!options.Enabled)
+                return builder;
+
+            builder.UseSwagger(c => c.RouteTemplate = "swagger/{documentName}/swagger.json");
+
+            return builder.UseSwaggerUI(c => c.SwaggerEndpoint($"/swagger/{options.Name}/swagger.json", options.Title));
+        }
+    }
+}
