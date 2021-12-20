b7f4f379f36d034abdc80712b0cd359434918389
stoyanov7 <georgi.stoyanov777@gmail.com>
2021-12-20 15:29:54 +0200

    discount - initialize repository, add method

---
+++ src/DistributedShop.Discounts/Repositories/DiscountRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace DistributedShop.Discounts.Repositories
+{
+    using DistributedShop.Common.Mongo.Contracts;
+    using DistributedShop.Discounts.Domain;
+    using System.Threading.Tasks;
+
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IMongoRepository<Discount> mongoRepository;
+
+        public DiscountRepository(IMongoRepository<Discount> mongoRepository)
+            => this.mongoRepository = mongoRepository;
+
+        public async Task AddAsync(Discount discount) => await this.mongoRepository.AddAsync(discount);
+    }
+}
---
+++ src/DistributedShop.Discounts/Repositories/IDiscountRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace DistributedShop.Discounts.Repositories
+{
+    using DistributedShop.Discounts.Domain;
+    using System.Threading.Tasks;
+
+    public interface IDiscountRepository
+    {
+        Task AddAsync(Discount discount);
+    }
+}
